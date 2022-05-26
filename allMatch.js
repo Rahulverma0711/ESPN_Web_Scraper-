19fcf200b3b098e7e485cfe536b84430312c5576
Rahulverma0711 <vermamm122@gmail.com>
2022-05-26 09:47:36 +0530

    project uploaded

---
+++ allMatch.js
@@ -0,0 +1,31 @@
+const request = require("request");
+const cheerio = require("cheerio");
+
+const { gifs } = require("./scorecards");
+function getAllMatch(url) {
+
+    request(url, cb);
+}
+function cb(err, res, body) {
+    if (err) {
+        console.error("error", err);
+    } else {
+        extractAllMatchLink(body);
+    }
+}
+function extractAllMatchLink(html) {
+    let selecTool = cheerio.load(html);
+    let scorecardElemArr = selecTool('a[data-hover="Scorecard"]');
+    console.log(scorecardElemArr.length);
+
+    for (let i = 0; i < scorecardElemArr.length; i++) {
+        let scorecardLink = selecTool(scorecardElemArr[i]).attr("href");
+        let fullLink = "https://www.espncricinfo.com" + scorecardLink;
+        gifs(fullLink);
+
+
+    }
+}
+module.exports = {
+    getAllMatch: getAllMatch,
+};
